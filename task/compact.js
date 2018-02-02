9297acde668c0d83ccd2a6e046c29e58ae1cdcc8
Your Name <you@example.com>
2018-02-02 20:32:30 +0000

    fix

--- task/compact.js
+++ task/compact.js
@@ -3,9 +3,7 @@ module.exports = function (gulp, plugins, config, setting) {
     return function (callback) {
         console.log('compact start');
         if (config.encode) {
-            return gulp.src(config.encode.map(function (file) {
-                return plugins.path.join(config.build, setting.sourse, file);
-            }))
+            return gulp.src(config.encode, {base: plugins.path.join(config.build, setting.sourse)})
                     .pipe(plugins.getTask('compact-file')())
                     .on('end', function () {
                         console.log('compact stop');
