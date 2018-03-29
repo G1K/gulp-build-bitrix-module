55af498b4fe5ab491d253530bfbdbb0967a19c6a
Jerff <info@jerff.ru>
2018-03-29 06:37:47 +0000

    убрал ненужное копирование из lang файла

--- task/diff.js
+++ task/diff.js
@@ -8,14 +8,6 @@ module.exports = function (gulp, plugins, config, setting) {
             }
             let path = [];
             let dist = new RegExp('(' + config + '\/)');
-
-            let lang = [];
-            plugins.fs.readdirSync('./lang/').map(function (file) {
-                let name = './lang/' + file + '/';
-                if (plugins.fs.statSync(name).isDirectory()) {
-                    lang.push(name);
-                }
-            });
             output.split(plugins.os.EOL).map((file) => {
                 if (!file.match(dist) && file != '' && plugins.fs.existsSync('./' + file)) {
                     let stats = plugins.fs.lstatSync('./' + file);
@@ -23,11 +15,6 @@ module.exports = function (gulp, plugins, config, setting) {
                         path.push(plugins.path.join(file, '**'));
                     } else {
                         path.push(file);
-                        lang.map(function(lang) {
-                            if(plugins.fs.existsSync(lang + file)) {
-                                path.push(lang + file);
-                            }
-                        });
                     }
                 }
             });
